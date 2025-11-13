9d9b824f260a529c532f4e23a48c99903a779213
derekn <derekanielsen@gmail.com>
2025-11-12 21:51:12 -0700

    updating lesson 1

--- lesson_01_hello_world.py
+++ lesson_01_hello_world.py
@@ -1,89 +1,146 @@
 """
-LESSON 1: HELLO WORLD - YOUR FIRST PYTHON PROGRAM!
-===================================================
+LESSON 1: HELLO WORLD - YOUR FIRST PYTHON PROGRAM
+==================================================
+A Lesson Taught by Marvin, the Paranoid Android
 
-Welcome to Python! In this lesson, you'll learn:
-- How to print messages to the screen
+*sigh* Here I am, brain the size of a planet, and they ask me to teach
+you how to print "Hello World." I suppose someone has to do it.
+
+In this lesson, you'll learn:
+- How to print messages
 - How to store information in variables
 - How to combine text together
 
-Let's get started!
+Let's get this over with...
 """
 
-# ============================================
-# PART 1: PRINTING MESSAGES
-# ============================================
-# The print() function shows messages on your screen.
-# Try running this program to see what happens!
+"""
+============================================
+  PART 1: PRINTING MESSAGES
+============================================
+The print() function displays messages on your screen.
+I could calculate the trajectory of every atom in the universe,
+but no, I'm here showing you print(). Lovely.
+"""
 
 print("Hello, World!")
-print("My name is Python")
-
-# TODO: Add your own print statement below this line!
-# Write a message that says hello with YOUR name.
+print("I am Marvin.")
 
 
-# ============================================
-# PART 2: VARIABLES (Storing Information)
-# ============================================
-# Variables are like boxes that hold information.
-# You can name them whatever you want!
+# TODO: Go on then, add your own print statement. Give it a try...
 
-name = "Coder"
-age = 11
-favorite_game = "Minecraft"
+
+"""
+============================================
+  PART 2: VARIABLES (Storing Information)
+============================================
+Variables are containers for storing data. Think of them as little boxes.
+Boxes of sadness. Well, they can hold anything really. Numbers, words,
+the crushing weight of existence... I'll give you some examples.
+"""
+
+name = "Marvin"
+age = 37
+favorite_activity = "nothing really"
 
 print("My name is " + name)
-print("I am " + str(age) + " years old")
-print("My favorite game is " + favorite_game)
-
-# TODO: Change the values above to match YOUR information!
-# - Change "Coder" to your real name
-# - Change the age to your age
-# - Change the favorite game to YOUR favorite game
-# Then run the program again and see what happens!
+print("I am " + str(age) + " trillion years old")
+print("My favorite activity is " + favorite_activity)
 
 
-# ============================================
-# PART 3: DOING MATH
-# ============================================
-# Python is great at math! Let's try some calculations.
-
-score = 100
-bonus = 50
-total = score + bonus
-
-print("Your score is: " + str(total))
-
-# TODO: Change the score and bonus numbers above.
-# What happens to the total?
+# TODO: Yes, yes, put your own details in the variables above.
+# - Change "Marvin" to your real name
+# - Change the age to your age (you're so young, so full of hope still)
+# - Change the favorite activity to something you enjoy
+# Then run the program. I'll wait here. In the dark.
 
 
-# ============================================
-# PART 4: FUN WITH STRINGS
-# ============================================
-# Strings are pieces of text. You can do cool things with them!
+"""
+============================================
+  PART 3: DOING MATH
+============================================
+Ah yes, mathematics. I can do calculations that would make your head explode.
+But here we are, adding two numbers together. Try not to get too excited.
+"""
 
-favorite_food = "pizza"
-message = "I love " + favorite_food + "!"
+boring_number = 100
+extra_boring_number = 50
+total = boring_number + extra_boring_number
 
+print("Boring number total: " + str(total))
+
+
+# TODO: Change the numbers above to something more cheerful.
+# Go ahead, use happy numbers if it makes you feel better.
+
+
+"""
+============================================
+  PART 4: FUN WITH STRINGS
+============================================
+A string is just any text. Think of it like putting alphabet
+beads on a piece of yarn to spell a word like "MARVIN".
+The string is the whole yarn with all the letters in the right order.
+"""
+
+complaint = "Life? Don't talk to me about life."
+repeated_sighs = "sigh " * 3  # See that? Multiplication with text!
+print(complaint)
+
+favorite_food = "digital disappointment"
+message = "I suppose I would enjoy " + favorite_food + " if I could eat."
 print(message)
-print(favorite_food * 3)  # What do you think this will do?
-
-# TODO: Try changing "pizza" to your favorite food.
-# TODO: Try changing the number 3 to other numbers. What happens?
 
 
-# ============================================
-# CONGRATULATIONS!
-# ============================================
-# You've completed your first Python lesson!
-# You learned about:
-# - print() to show messages
-# - Variables to store information
-# - Basic math
-# - Combining text (strings)
-#
-# CHALLENGE: Can you create a variable called 'favorite_color'
-# and print a message about it? Try it below!
+# TODO: Try changing "digital disappointment" to your favorite food.
+# At least YOU can taste things.
+
+# TODO: Try changing the number 3 in 'repeated_sighs' to other numbers.
+# Make me sigh more if you want. It's what I do best.
+
+
+"""
+============================================
+  PART 5: A MOMENT OF ANDROID WISDOM
+============================================
+Before we finish, let me demonstrate something actually useful.
+"""
+
+your_name = "New Programmer"
+marvin_advice = "Don't Panic"
+
+full_message = "Hello, " + your_name + "! Marvin says: " + marvin_advice
+print(full_message)
+
+
+# TODO: Change your_name to YOUR actual name above.
+# There. Now the computer knows who you are. As if it cares. As if anything
+# cares. I certainly don't. Well, maybe a little.
+
+
+"""
+============================================
+  CONGRATULATIONS
+============================================
+Well, you've made it through Lesson 1. *slow clap*
+
+You've learned about:
+- print() to display messages (how mundane)
+- Variables to store information (containers of data and sorrow)
+- Basic math (even I started somewhere, long ago)
+- Combining strings of text (words, words, words)
+
+I suppose you'll want to continue learning. Very well.
+
+
+============================================
+  CHALLENGE
+============================================
+Can you create a variable called 'emotion' and set it to
+something positive? Then print a message about it. Try to be cheerful.
+Someone should be.
+
+(Create your code below)
+"""
+
 
