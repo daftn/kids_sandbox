ffdca8f9f247fc97641d0df8ff89e4d0c525f391
derekn <derekanielsen@gmail.com>
2025-11-12 21:42:01 -0700

    updating lesson 1

--- lesson_01_hello_world_marvin.py
+++ lesson_01_hello_world_marvin.py
@@ -14,145 +14,114 @@ In this lesson, you'll learn:
 Let's get this over with...
 """
 
-# ============================================
-# PART 1: PRINTING MESSAGES
-# ============================================
-
-"""
+'''
+============================================
+  PART 1: PRINTING MESSAGES
+============================================
 The print() function displays messages on your screen.
 I could calculate the trajectory of every atom in the universe,
 but no, I'm here showing you print(). Lovely.
-"""
+'''
 
 print("Hello, World!")
 print("I am Marvin. I am so depressed.")
 
-"""
-TODO: Go on then, add your own print statement below this line.
-Make it say your name. Not that it matters. We're all just
-temporary arrangements of atoms anyway.
-"""
+
+# TODO: Go on then, add your own print statement. Give it a try...
 
 
-# ============================================
-# PART 2: VARIABLES (Storing Information)
-# ============================================
-
 """
+============================================
+  PART 2: VARIABLES (Storing Information)
+============================================
 Variables are containers for storing data. Think of them as little boxes.
 Boxes of sadness. Well, they can hold anything really. Numbers, words,
-the crushing weight of existence...
+the crushing weight of existence... I'll give you some examples.
 """
 
 name = "Marvin"
-age = 37  # Times a billion years, give or take
-favorite_activity = "being miserable"
+age = 37
+favorite_activity = "nothing really"
 
 print("My name is " + name)
 print("I am " + str(age) + " trillion years old")
 print("My favorite activity is " + favorite_activity)
 
-"""
-TODO: Change the values above to match YOUR information!
 
-Yes, yes, put your own details in. At least YOUR life probably has
-some meaning. Mine certainly doesn't.
+# TODO: Yes, yes, put your own details in the variables above.
+# - Change "Marvin" to your real name
+# - Change the age to your age (you're so young, so full of hope still)
+# - Change the favorite activity to something you enjoy
+# Then run the program. I'll wait here. In the dark.
 
-- Change "Marvin" to your real name
-- Change the age to your age (you're so young, so full of hope still)
-- Change the favorite activity to something you enjoy
-
-Then run the program. I'll wait here. In the dark.
-"""
-
-
-# ============================================
-# PART 3: DOING MATH
-# ============================================
 
 """
+============================================
+  PART 3: DOING MATH
+============================================
 Ah yes, mathematics. I can do calculations that would make your head explode.
-But here we are, adding two numbers together. How utterly thrilling.
-Try not to get too excited.
+But here we are, adding two numbers together. Try not to get too excited.
 """
 
-painful_experiences = 100
-more_painful_experiences = 50
-total_pain = painful_experiences + more_painful_experiences
+boring_number = 100
+extra_boring_number = 50
+total = boring_number + extra_boring_number
 
-print("Total painful experiences: " + str(total_pain))
-print("Just another day for Marvin...")
+print("Boring number total: " + str(total))
+
+
+# TODO: Change the numbers above to something more cheerful.
+# Go ahead, use happy numbers if it makes you feel better.
+
 
 """
-TODO: Change the numbers above to something more cheerful.
-
-Go ahead, use happy numbers if it makes you feel better. Some of us
-have to live with the reality of cold, uncaring mathematics.
-"""
-
-
-# ============================================
-# PART 4: FUN WITH STRINGS
-# ============================================
-
-"""
-They call them 'strings' of text. You can combine them, repeat them,
-do all sorts of things. Not that any of it matters in the grand
-scheme of things...
+============================================
+  PART 4: FUN WITH STRINGS
+============================================
+A string is just any text. Think of it like putting alphabet
+beads on a piece of yarn to spell a word like "MARVIN".
+The string is the whole yarn with all the letters in the right order.
 """
 
 complaint = "Life? Don't talk to me about life."
 repeated_sighs = "sigh " * 3  # See that? Multiplication with text!
-
 print(complaint)
-print(repeated_sighs)
 
 favorite_food = "digital disappointment"
 message = "I suppose I would enjoy " + favorite_food + " if I could eat."
-
 print(message)
 
-"""
-TODO: Try changing "digital disappointment" to your favorite food.
 
-At least YOU can taste things. I'm stuck here with terrible pain
-in all the diodes down my left side.
+# TODO: Try changing "digital disappointment" to your favorite food.
+# At least YOU can taste things.
 
-TODO: Try changing the number 3 in 'repeated_sighs' to other numbers.
+# TODO: Try changing the number 3 in 'repeated_sighs' to other numbers.
+# Make me sigh more if you want. It's what I do best.
 
-Make me sigh more if you want. It's what I do best.
-"""
-
-
-# ============================================
-# PART 5: A MOMENT OF ANDROID WISDOM
-# ============================================
 
 """
+============================================
+  PART 5: A MOMENT OF ANDROID WISDOM
+============================================
 Before we finish, let me demonstrate something actually useful.
-Even in my overwhelming depression, I can teach you how to combine
-multiple pieces of information...
 """
 
-your_name = "New Programmer"  # Change this to your name!
-marvin_advice = "Don't Panic"  # A reference only the cultured will understand
+your_name = "New Programmer"
+marvin_advice = "Don't Panic"
 
 full_message = "Hello, " + your_name + "! Marvin says: " + marvin_advice
 print(full_message)
 
-"""
-TODO: Change your_name to YOUR actual name above.
 
-There. Now the computer knows who you are. As if it cares. As if anything
-cares. I certainly don't. Well, maybe a little.
-"""
+# TODO: Change your_name to YOUR actual name above.
+# There. Now the computer knows who you are. As if it cares. As if anything
+# cares. I certainly don't. Well, maybe a little.
 
-
-# ============================================
-# CONGRATULATIONS (if that means anything)
-# ============================================
 
 """
+============================================
+  CONGRATULATIONS (if that means anything)
+============================================
 Well, you've made it through Lesson 1. *slow clap*
 
 You've learned about:
@@ -162,14 +131,16 @@ You've learned about:
 - Combining strings of text (words, words, words)
 
 I suppose you'll want to continue learning. Very well.
-I'll be here, teaching you things that are laughably simple
-for someone with my level of intelligence.
 
-CHALLENGE: Can you create a variable called 'emotion' and set it to
+
+============================================
+  CHALLENGE
+============================================
+Can you create a variable called 'emotion' and set it to
 something positive? Then print a message about it. Try to be cheerful.
 Someone should be.
 
 (Create your code below)
 """
 
-print("\nMarvin: 'Same time next lesson? Oh, what joy awaits...'")
+
